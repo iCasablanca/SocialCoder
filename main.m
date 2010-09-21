8806f894192e2eed9eb4d2180ef996617f3ead85
Toni Suter <tonisuter@me.com>
2010-09-21 20:41:50 +0200

    initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SocialCoder
+//
+//  Created by Toni Suter on 21.09.10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
+}
